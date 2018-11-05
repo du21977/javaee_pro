568dcb1013962b298174c6846535525c4ef76cee
dobi <549914283@qq.com>
2018-11-05 22:33:20 +0800

    Spring3全注解方式创建对象

--- src/main/java/com/dobi/controller/HelloController.java
+++ src/main/java/com/dobi/controller/HelloController.java
@@ -1,12 +1,19 @@
 package com.dobi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.sql.DataSource;
+
 @Controller
 public class HelloController {
+
+    @Autowired
+    private DataSource dataSource;
+
 
     @GetMapping("hello")
     @ResponseBody
